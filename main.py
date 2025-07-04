1d58fd7e0e6100d7567052cc44cd9313d05ccebe
unknown <gianenabore30@gmail.com>
2025-07-04 23:01:38 +0800

    application's main entry point

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ from gui import App
 if __name__ == "__main__":
     app = App()
     app.mainloop()
+
+"""
+before running the code, make sure to put the assets folder in your C:\Users\<User>  directory
+for it will be responsible for the images and other assets used in the application.
+"""
